5f9955be4d67ec664e2367e1498b728547f9cb0b
Clowcadia <andrei.pak.1989@gmail.com>
2017-03-13 23:22:39 -0400

    Updated With Prompts

---
+++ 1.11/NPCTesting/src/main/java/com/clowcadia/test/GuiHandler.java
@@ -0,0 +1,47 @@
+package com.clowcadia.test;
+
+
+import java.security.PublicKey;
+
+
+import com.clowcadia.test.containers.ContainerBasic;
+import com.clowcadia.test.gui.GuiBasic;
+import com.clowcadia.test.utils.Utils;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.datafix.fixes.EntityId;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.IGuiHandler;
+
+public class GuiHandler implements IGuiHandler{
+	
+	
+	public static final int TEST = 1;	
+
+	@Override
+	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		Utils.getLogger().info("GuiHandler: Constructor");
+		
+		if(ID==TEST){
+			Entity test = world.getEntityByID(x);
+			
+			return new ContainerBasic(player.inventory,test);
+		}
+		return null;
+	}
+
+	@Override
+	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		Utils.getLogger().info("GuiHandler: getClientGuiElement");
+		
+		if(ID==TEST){
+			Entity test = world.getEntityByID(x);
+			return new GuiBasic(player.inventory, test);
+		}
+		return null;
+	}
+
+}
